d050fff6caf7b687571ab32dedefa98e0f93ef0a
Joy <joychang97@gmail.com>
2022-04-11 21:10:50 +0800

    0411_新增作業2

---
+++ MyHW/2. FrmCategoryProducts.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyHomeWork
+{
+    public partial class FrmCategoryProducts : Form
+    {
+        public FrmCategoryProducts()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            //step1: SqlConnection
+            SqlConnection conn = null;  //宣告conn變數
+
+            try
+            {
+                conn = new SqlConnection("Data Source=.;Initial Catalog=Northwind;Integrated Security=True");
+                conn.Open();
+
+                //step2: SqlCommand
+                string commandText = $"select ProductName from Products as p join Categories as c on p.CategoryID = c.CategoryID where CategoryName = '{comboBox1.Text}'";
+                SqlCommand command = new SqlCommand(commandText, conn);
+
+                //step3: SqlDataReader
+                SqlDataReader dataReader = command.ExecuteReader();
+                
+                this.listBox1.Items.Clear();
+                while (dataReader.Read()) 
+                {
+                    string s = $"{dataReader["ProductName"]}";
+                    this.listBox1.Items.Add(s);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally 
+            {
+                if (conn != null) 
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿select  c.CategoryID, CategoryName, ProductName
+from Products as p
+join Categories as c
+on p.CategoryID = c.CategoryID
+where CategoryName='Beverages'
